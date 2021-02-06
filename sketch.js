5027526670201f0e4e7f36cdaba926e8b210d037
ayush713 <arjyadiptaojha1111@gmail.com>
2021-02-06 04:00:10 -0800

    cat

--- sketch.js
+++ sketch.js
@@ -11,18 +11,18 @@ function preload() {
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(12000,1400);
     engine = Engine.create();
     world = engine.world;
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(200, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    box1 = new Box(704,320,70,70);
+    box2 = new Box(920,320,70,71);
+    pig1 = new Pig(800, 350);
+    log1 = new Log(811,260,310, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
